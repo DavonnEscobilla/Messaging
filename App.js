c4a40c969c6a2cd79d61be117e39f45b500b46ef
Davonn Escobilla <qdpescobilla@tip.edu.ph>
2023-11-21 15:56:51 +0800

    Procedure

--- App.js
+++ App.js
@@ -1,38 +1,45 @@
-import { StyleSheet, Text, View } from 'react-native';
-import Status from './components/Status';
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import MessageList from "./components/MessageList";
+import { createImageMessage, createLocationMessage, createTextMessage } from "./utils/MessageUtils";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Status />
-    </View>
-  );
+class Message extends Component {
+  state = {
+    messages: [
+      createImageMessage('https://unsplash.it/300/300'),
+      createTextMessage('World'),
+      createTextMessage('Hello'),
+      createLocationMessage({
+        latitude: 37.78825,
+        longitude: -122.4324,
+      }),
+    ],
+  };
+
+  handlePressMessage = (message) => {
+    console.log('Message pressed:', message);
+  }
+
+  renderMessageList() {
+    const { messages } = this.state;
+
+    return (
+      <View style={styles.content}>
+        <MessageList messages={messages} onPressMessage={this.handlePressMessage} />
+      </View>
+    );
+  }
+
+  render() {
+    return this.renderMessageList();
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const styles = {
   content: {
     flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#F8EDE3'
   },
-  inputMethodEditor: {
-    flex: 1,
-    backgroundColor: 'white',
-  },
-  toolbar: {
-    borderTopWidth: 1,
-    borderTopColor: 'rgba(0,0,0,0.04)',
-    backgroundColor: 'white',
-  },
-  androidStatusBar: { 
-    barStyle: "dark-content", 
-    backgroundColor: "#FFFFFF" 
-  },
-});
+};
+
+export default Message;
